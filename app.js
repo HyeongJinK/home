bc69545ec02df7a9d51e34a85501b025889d1b14
estgames <wkzkfmxk23@gmail.com>
2018-06-27 17:28:03 +0900

    뷰 설정

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var packBookRouter = require('./routers/packBook/index');
+var packBookRouter = require('./routes/packBook/index');
 var wikiRouter = require('./routes/wiki/index');
-var youtubeRouter = require('/routes/youtube/index');
+var youtubeRouter = require('./routes/youtube/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.set('layout', 'layout');
+app.set("layout extractScripts", true);
 
 app.use(logger('dev'));
 app.use(express.json());
