11c379c16a2cf56e3acf64d5a41fc7a15a74ffb4
estgames <wkzkfmxk23@gmail.com>
2018-09-06 18:01:05 +0900

    원서에서 md파일로 변환 작업중

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var packBookListRouter = require('./routes/packBook/list');
-var wikiRouter = require('./routes/wiki/index');
-var youtubeRouter = require('./routes/youtube/index');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const packBookListRouter = require('./routes/packBook/list');
+const mdRouter = require('./routes/md/list');
+const wikiRouter = require('./routes/wiki/index');
+const youtubeRouter = require('./routes/youtube/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +32,7 @@ app.use('/users', usersRouter);
 app.use('/pack', packBookListRouter);
 app.use('/wiki', wikiRouter);
 app.use('/youtube', youtubeRouter);
+app.use('/md', mdRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/md/list.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var sqlite3 = require('sqlite3').verbose();
+var router = express.Router();
+
+const mdPath = 'db/md.db';
+const bookDBPath = 'db/books.db';
+
+router.get('/', function(req, res, next) {
+  res.render('md/list', {});
+});
+
+router.get('/list', function(req, res, next) {
+  let db = new sqlite3.Database(mdPath);
+  
+  let pageNum = req.query.pageNum;
+  let rowNum = req.query.rowNum;
+  let startNum = (pageNum - 1) * rowNum;
+  let searchText = req.query.searchText;
+  
+  let sql = "SELECT * FROM book_title";
+  let countSql = "SELECT count(*) as bookCount FROM book_title";
+  let param = [];
+  let countParam = [];
+  if (searchText != "") {
+    searchText = "%"+searchText+"%"
+    sql += " WHERE title like ?";
+    param.push(searchText);
+    countSql += " WHERE title like ?";
+    countParam.push(searchText);
+  }
+  //sql += " ORDER BY publicationDate desc LIMIT ?, ?"
+  param.push(startNum);
+  param.push(rowNum);
+
+  db.all(sql, param, function(err, rows) {
+    db.get(countSql, countParam, (err, row) => {
+      res.send({"books" : rows, "bookCount" : row.bookCount});
+    });  
+  });	 
+  db.close();
+});
+
+router.get('/create/en/:isbn', function(req, res,next) {
+    let isbn = req.params.isbn;
+    let bookdb = new sqlite3.Database(bookDBPath);
+    let mddb = new sqlite3.Database(mdPath);
+    db.all("SELECT * FROM content WHERE isbn = ? ORDER BY contentIndex", [req.params.isbn], function(err, rows) {
+        console.log(rows);
+        //TODO md로 파싱, db 저장
+        //res.send({"book_data" : rows})
+    });
+    db.close();
+});
+
+
+
+module.exports = router;
