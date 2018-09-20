447736b050cf1980f9a3bbb005cd8c19ced78c7d
estgames <wkzkfmxk23@gmail.com>
2018-09-20 18:02:21 +0900

    asdkf

--- routes/youtube/index.js
+++ routes/youtube/index.js
@@ -8,27 +8,63 @@ router.get('/', function(req, res, next) {
   res.render('youtube', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/down', function(req, res, next) {
+
+});
+
+router.get('/downlist', function(req, res, next) {
+
+});
+
+
+
+
+
+function playone(url) {
+  var video = youtubedl(url,
+  ['--format=18'],
+  { cwd: __dirname });
+
+let size = 0;
+video.on('info', function(info) {
+  size = info.size;
+  video.pipe(fs.createWriteStream("./download/"+info._filename));  
+});
+
+let pos = 0;
+video.on('data', function data(chunk) {
+  pos += chunk.length;
+  if (size) {
+    var percent = (pos / size * 100).toFixed(2);
+    process.stdout.cursorTo(0);
+    process.stdout.clearLine(1);
+    process.stdout.write(percent + '%');
+  }
+});
+ 
+video.on('end', function() {                                                                                                                                                                                                                                                                                                                                                    
+  console.log('finished downloading!');
+});
+}
 //playlist('https://www.youtube.com/playlist?list=PLFoH8spXdgNHUPpEhqktU9IPawKXFpn4B');
 function playlist(url) {
- 
   'use strict';
   var video = ytdl(url);
- 
+
   video.on('error', function error(err) {
     console.log('error 2:', err);
   });
- var size = 0;
+  var size = 0;
   video.on('info', function(info) {
     size = info.size;
     console.log("\n" + info._filename);
     video.pipe(fs.createWriteStream("./download/"+info._filename));
   });
- 
+
   var pos = 0;
   video.on('data', function data(chunk) {
     pos += chunk.length;
-    // `size` should not be 0 here.
+    
     if (size) {
       var percent = (pos / size * 100).toFixed(2);
       process.stdout.cursorTo(0);
@@ -36,6 +72,8 @@ function playlist(url) {
       process.stdout.write(percent + '%');
     }
   });
- 
+
   video.on('next', playlist); 
 }
+
+module.exports = router;
