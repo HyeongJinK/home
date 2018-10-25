92bd2003bb6ecba15dcb4e22ef179d0e03da87aa
estgames <wkzkfmxk23@gmail.com>
2018-10-25 17:56:26 +0900

    6

--- routes/packBook/test.js
+++ routes/packBook/test.js
@@ -28,8 +28,13 @@ query.selectBookReservationByFinish(db, (rows) => {
 db.close();
 
 /**
- * 
- * 
+ * CREATE TABLE "book" ( `isbn` TEXT NOT NULL, `title` TEXT NOT NULL, `publicationDate` TEXT, `imgUrl` TEXT, `category` TEXT, `description` TEXT, `mainType` TEXT, PRIMARY KEY(`isbn`) )
+ * CREATE TABLE "bookReservation" ( `idx` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `isbn` TEXT NOT NULL, `finish` TEXT NOT NULL DEFAULT 'false', `save` INTEGER NOT NULL DEFAULT 0, `orderNum` INTEGER )
+ * CREATE TABLE "bookTranslate" ( `isbn` TEXT )
+ * CREATE TABLE "content" ( `isbn` TEXT NOT NULL, `menuNum` TEXT, `contentIndex` INTEGER NOT NULL, `title` TEXT, `content` TEXT, PRIMARY KEY(`isbn`,`contentIndex`) )
+ * CREATE TABLE `contentTranslate` ( `isbn` TEXT, `menuNum` TEXT, `contentIndex` INTEGER, `title` TEXT, `content` TEXT, PRIMARY KEY(`isbn`,`contentIndex`) )
+ * CREATE TABLE `errorLog` ( `idx` INTEGER PRIMARY KEY AUTOINCREMENT, `isbn` TEXT NOT NULL, `contentIdx` TEXT NOT NULL, `regDate` TEXT NOT NULL )
+ * CREATE TABLE sqlite_sequence(name,seq)
  */
 // query.insertBookReservation(db, (err) => {
 //     if (err)
