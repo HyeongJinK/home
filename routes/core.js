67bb6f795cbf0ddcb3ad4c211784ac55442630f9
estgames <wkzkfmxk23@gmail.com>
2018-11-09 18:03:19 +0900

    table info

--- routes/core.js
+++ routes/core.js
@@ -15,8 +15,35 @@ router.post('/sql', function(req, res, next) {
     if (err) {
       console.log(err);
     }
-    console.log(rows)
+    //console.log(rows)
     res.send({"sql" : sql, "err" : err, "result": rows});
+  })
+  db.close();
+});
+
+router.get("/db", function (req, res, next) {
+  let db = new sqlite3.Database(bookDBPath);
+
+  db.all("SELECT * FROM sqlite_master", (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+    //console.log(rows)
+    res.send({"err" : err, "result": rows});
+  })
+  db.close();
+});
+
+router.get("/columns", function (req, res, next) {
+  let db = new sqlite3.Database(bookDBPath);
+  let tableName = req.query.tableName
+  console.log(tableName)
+  db.all("PRAGMA table_info("+tableName+")", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+    //console.log(rows)
+    res.send({"err" : err, "result": rows});
   })
   db.close();
 });
