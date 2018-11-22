49a339116bdbde4096b2db1aed880cdb79762af5
estgames <wkzkfmxk23@gmail.com>
2018-11-22 15:51:37 +0900

    이동한 파일 적용

--- routes/query/board.js
+++
@@ -1,5 +0,0 @@
-exports.save = function(db, callBack) {
-    db.run("INSERT INTO board (boardIdx, title, content, createDate, modifyDate, hidden) VALUES (?,?,?,?,?,?)", data, function(err) {
-        callBack(err);
-    }); 
-};
