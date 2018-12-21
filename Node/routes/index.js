525d7a79c6b3d3c4a5c9b685c85c73e77ed6da00
estgames <wkzkfmxk23@gmail.com>
2018-12-21 16:38:40 +0900

    첫화면 에러 수정

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index_new', { title: '어떻게 오셨데요?' });
+  res.render('index_new', { title: '어떻게 오셨데요?' , menu: ""});
 });
 
 module.exports = router;
