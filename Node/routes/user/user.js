03146bda13daf5c0e214370e8f3e0d2cd52aeb02
estgames <wkzkfmxk23@gmail.com>
2019-01-04 11:11:11 +0900

    로그인&코어

--- Node/routes/user/user.js
+++ Node/routes/user/user.js
@@ -1,3 +1,13 @@
+/**
+CREATE TABLE `users` (
+	`idx`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`email`	TEXT NOT NULL UNIQUE,
+	`password`	TEXT NOT NULL,
+	`nickname`	TEXT NOT NULL,
+	`createDate`	TEXT
+);
+ */
+
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
