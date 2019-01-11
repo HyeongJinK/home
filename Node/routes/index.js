ff750d97b3b12cb31ea892368e99c6a169dd9993
webplatform ESTgames <webplatformestgames@wkzkfmxk12.13.20.172.in-addr.arpa>
2019-01-11 17:54:48 +0900

    메뉴 추가

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -5,7 +5,7 @@ console.info("Index Route...")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index_new', { title: '어떻게 오셨데요?' , menu: ""});
+  res.render('./index_new', { title: '어떻게 오셨데요?' , menu: ["메인화면"]});
 });
 
 module.exports = router;
