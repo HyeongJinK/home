4a5830fb34d73026549827ddb3ffd4d57bcdd1a5
unknown <wkzkfmxk23@gmail.com>
2019-02-22 23:29:20 +0900

    일감 리스트 출력 컬럼 수정

--- Node/routes/project/db/sql/task.js
+++ Node/routes/project/db/sql/task.js
@@ -1,10 +1,30 @@
 exports.sql = {
     findByAll : `
-        SELECT
-            *
-        FROM
-            task
-        ORDER BY idx DESC`
+    SELECT
+        ta.idx as idx
+        , ta.title
+        , ta.start_time
+        , ta.finish_time
+        , pr.title as projectName
+        , ty.name as typeName
+        , st.name as statusName
+        , pri.name as priorityName
+        , ta.manager
+        , ta.progress
+        , ve.title as versionName
+    FROM
+        task as ta
+    left join project as pr
+        on ta.projectIdx = pr.idx
+    left join type as ty
+        on ta.type = ty.idx
+    left join status as st
+        on ta.status = st.idx
+    left join priority as pri
+        on ta.priority = pri.idx
+    left join version as ve
+        on ta.versionIdx = ve.idx
+    ORDER BY idx DESC`
     , findByIdx: `
         SELECT
             *
